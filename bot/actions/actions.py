8039a6691a7c73cd73a4a47b2f5af2028a875876
guilacerda <guiglacerda@gmail.com>
2019-06-10 10:18:55 -0300

    Gets user message
    Signed-off-by: guilacerda <guiglacerda@gmail.com>

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -10,3 +10,13 @@ class ActionTest(Action):
             dispatcher.utter_message("Mensagem enviada por uma custom action.")
         except ValueError:
             dispatcher.utter_message(ValueError)
+
+class ActionFallback(Action):
+    def name(self):
+        return "action_fallback"
+
+    def run(self, dispatcher, tracker, domain):
+        text = ''
+        text = tracker.latest_message.get('text')
+
+        dispatcher.utter_message(text)
