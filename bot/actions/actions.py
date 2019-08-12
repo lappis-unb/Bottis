dcf723e3569506ee9f2c24904621243a2d46888e
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-08-12 11:08:17 -0300

    Add methods to stream message to broker server
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -40,19 +40,50 @@ class ActionFallback(Action):
     def name(self):
         return "action_fallback"
 
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, text):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='rpc_queue',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,
+            ),
+            body=text)
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
+
+    def __init__(self):
+        self.connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=env.str("BROKER_URL", "")))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+
     def run(self, dispatcher, tracker, domain):
         text = ''
         text = tracker.latest_message.get('text')
 
-        bots = get_bots_from_env()
         # TODO: Inserção das API's sem ser hardcode
         # bots = ["localhost:5006", 'localhost:5007']
 
         # TODO: Paralelizar o envio das mensagens para as APIs cadastradas
         # TODO: Configurar os dados que recebemos do tracker em uma struct separada
-        answers = self.ask_bots(text, bots)
+        answer = self.call(text)
 
-        answer = self.get_best_answer(answers)
         # TODO: Continuar com o Fallback padrão quando nenhum bot tem confiança suficiente
         logger.info("\n\n -- Answer Selected -- ")
         logger.info("Bot: " + answer["bot"])
