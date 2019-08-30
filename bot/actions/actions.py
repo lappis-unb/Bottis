45f9b1ee943198bfc6f17aa8223265b861a29328
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-08-30 16:42:58 -0300

    Add CustomResponse action
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -10,3 +10,12 @@ class ActionTest(Action):
             dispatcher.utter_message("Mensagem enviada por uma custom action.")
         except ValueError:
             dispatcher.utter_message(ValueError)
+
+class ActionCustomResponse(Action):
+    def name(self):
+        return "action_custom_response"
+
+    def run(self, dispatcher, tracker, domain):
+        text = tracker.get_slot('bot_answer')
+
+        dispatcher.utter_attachment(str(text))
