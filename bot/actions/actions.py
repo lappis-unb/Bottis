3a9d4ae560a760117750787e40382cd87539238a
Pablo Diego <pablodiegoss@hotmail.com>
2019-09-11 12:04:06 -0300

    Fix missing actions and requirements

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -1,3 +1,7 @@
+from rasa_core_sdk import Action
+from rasa_core.events import SlotSet
+
+
 class ActionTest(Action):
     def name(self):
         return "action_test"
@@ -8,4 +12,12 @@ class ActionTest(Action):
         except ValueError:
             dispatcher.utter_message(ValueError)
 
-logger = logging.getLogger(__name__)
+class ActionCustomResponse(Action):
+    def name(self):
+        return "action_custom_response"
+
+    def run(self, dispatcher, tracker, domain):
+        messages = tracker.get_slot('bot_answers')
+
+        for message in messages:
+            dispatcher.utter_message(message)
